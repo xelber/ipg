8d850a8ed930d735e04397efae835eb891b32ed3
Hiran Milinda <hiran.milinda@au.harveynorman.com>
2017-07-17 18:57:09 +1000

    Trailing mailer, proof of concept

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactUs;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Mail;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test()
+    {
+		Mail::to('hiranmilinda@gmail.com')->send( new ContactUs() );
+    }
 }
